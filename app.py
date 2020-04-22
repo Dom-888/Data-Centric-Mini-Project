64247f108606b221ddce523c307b1f9e2b352484
Dom-888 <spinelladomenico88@gmail.com>
2020-04-22 10:57:35 +0000

    Bind select element to Mongo database

--- app.py
+++ app.py
@@ -11,14 +11,15 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+# @app.route('/get_tasks')
+# def get_tasks():
+#     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html',
+    categories=mongo.db.categories.find()) # Passa le categorie del database nel select element in addtask.html
 
 
 if __name__ == '__main__':
