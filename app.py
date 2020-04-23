9600ae71007190277a5c173c4ce30dd1961c1728
Dom-888 <spinelladomenico88@gmail.com>
2020-04-23 11:00:39 +0000

    Add update_task

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId # Necessario alla funzione edit_task
+from bson.objectid import ObjectId # Necessary to the edit_task function
 
-# Collega il database al repo
+# Connect the database to the repo
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-# Substitute with an enviroment variable
-app.config["MONGO_URI"] = 'mongodb+srv://RK3n:cimpalimpa089@cluster0-1hvju.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://RK3n:cimpalimpa089@cluster0-1hvju.mongodb.net/task_manager?retryWrites=true&w=majority' # Substitute with an enviroment variable
 
 mongo = PyMongo(app)
 
 
-@app.route('/')
+@app.route('/') # Determine the landing page
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
@@ -21,25 +20,38 @@ def get_tasks():
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
-    categories=mongo.db.categories.find())  # La variabile categories contiene i risultati della query categories.find(). E' possibile estrarli con un for loop
+    categories=mongo.db.categories.find())  # The  variable 'categories" contains the results of the categories.find () query. It is possible to extract them with a for loop
 
-# Attiva l'add task button 
-@app.route('/insert_task', methods=['POST']) # Insert task va inserito nel form tag con jinja
+# Activate the add task button 
+@app.route('/insert_task', methods=['POST']) # Insert task must be inserted in the form tag with jinja
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
-# Attiva l'edit button
+#  Activate the edit button
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
     the_task = mongo.db.task.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task, categories=all_categories)
 
+# Update the task in the database
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)},
+    {
+        'task_name': request.form.get('task_name'),
+        'category_name': request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent': request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks'))
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
